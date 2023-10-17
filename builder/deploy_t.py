8a8c135ecf77ea44ccd326a3a27851dfa7bf2d78
dapangyu <zhaoyihuan@bilibili.com>
2023-10-17 11:10:43 +0800

    test

---
+++ builder/deploy_t.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+db_master = mysql.connector.connect(
+  host="172.88.88.3",
+  user="root",
+  password="password"
+)
+
+db_slave_1 = mysql.connector.connect(
+  host="172.88.88.4",
+  user="root",
+  password="password"
+)
+
+db_slave_2 = mysql.connector.connect(
+  host="172.88.88.5",
+  user="root",
+  password="password"
+)
+
+print(db_master)
+print(db_slave_1)
+print(db_slave_2)
